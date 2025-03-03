cb4065e248f4f303ccc650af90366b4b2d813a5d
Boltaboyev Sarvarbek <boltaboyev708@gmail.com>
2025-03-03 15:25:29 +0500

    last

--- challenges/last.sql
+++ challenges/last.sql
@@ -1,10 +1,14 @@
 USE cinema_booking_system;
  
 -- How many of our customers have made a booking.
+SHOW TABLES;
+SELECT c.first_name, c.last_name, COUNT(b.customer_id) FROM customers c
+JOIN bookings b ON b.customer_id = c.id
+GROUP BY c.first_name, c.last_name;
 
-SELECT count(*) FROM customers;
-
-SELECT * FROM bookings
-ORDER BY customer_id;
-
-SELECT count(distinct(customer_id)) AS no_of_customers FROM bookings;
+SELECT COUNT(*) as clients FROM
+(
+SELECT c.first_name, c.last_name, COUNT(b.customer_id) FROM customers c
+JOIN bookings b ON b.customer_id = c.id
+GROUP BY c.first_name, c.last_name
+) a ;
